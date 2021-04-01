1b54db535957839b67379f7d1a355e746fcb7985
sawood14012 <msawood@redhat.com>
2021-04-01 14:32:54 +0530

    migrate ci to actions 2

--- runtest.sh
+++ runtest.sh
@@ -58,9 +58,8 @@ here=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 export PYTHONPATH=${here}/
 
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
-pip3 install --ignore-installed git+https://github.com/fabric8-analytics/fabric8-analytics-rudra.git@4cfac114208cd0f062a1d7ab0e03bb6bc7a490a3#egg=rudra
+pip install -r requirements.txt
+pip install --ignore-installed git+https://github.com/fabric8-analytics/fabric8-analytics-rudra.git@4cfac114208cd0f062a1d7ab0e03bb6bc7a490a3#egg=rudra
 
 
 echo "*****************************************"
